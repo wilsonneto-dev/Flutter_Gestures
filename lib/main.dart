f3358256ddacc286ba02c915fec33b4404a555b4
Wilson Neto Dev <contato@wilsonneto.com.br>
2020-02-20 10:10:40 -0300

    last module

---
+++ lib/main.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  int numTaps = 0;
+  int numDoubleTaps = 0;
+  int numLongPress = 0;
+
+  double posX = 0.0;
+  double posY = 0.0;
+
+  double boxSize = 150.0;
+
+  @override
+  Widget build(BuildContext context) {
+    if(posX == 0.0) {
+      center(context);
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Gestures"),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          setState(() {
+            numTaps++;
+          });
+        },
+        onDoubleTap: () {
+          setState(() {
+            numDoubleTaps++;
+          });
+        },
+        onLongPress: () {
+          setState(() {
+            numLongPress++;
+          });
+        },
+        onVerticalDragUpdate: (DragUpdateDetails value) {
+          setState(() {
+            double delta = value.delta.dy;
+            posY+= delta;
+          });
+        },
+        onHorizontalDragUpdate: (DragUpdateDetails value) {
+          setState(() {
+            double delta = value.delta.dx;
+            posX += delta;            
+          });
+        },
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              left: posX,
+              top: posY,
+              child: Container(
+                width: boxSize,
+                height: boxSize,
+                decoration: BoxDecoration(
+                  color: Colors.red
+                ),
+              ),
+            )
+          ],
+        )
+      ),
+      bottomNavigationBar: Material(
+        color: Theme.of(context).primaryColorLight,
+        child: Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Text(
+            "Taps: $numTaps - Double Tabs $numDoubleTaps - Long Press $numLongPress",
+            style: Theme.of(context).textTheme.title,
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void center(BuildContext context) {
+    posX = (MediaQuery.of(context).size.width / 2 ) - boxSize/2;
+    posY = (MediaQuery.of(context).size.height / 2 ) - boxSize/2;
+    setState(() {
+      posX = posX;
+      posY = posY;
+    });
+  }
+
+}
